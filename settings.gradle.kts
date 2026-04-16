9cf131e5988824e19cf5676f9b48385a666889df
fikrimilano <fikrimilano1@gmail.com>
2026-04-16 13:04:11 +0700

    Setup gradle and KMP structure

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+rootProject.name = "kotlin-fhir-engine"
+
+include(":engine")
