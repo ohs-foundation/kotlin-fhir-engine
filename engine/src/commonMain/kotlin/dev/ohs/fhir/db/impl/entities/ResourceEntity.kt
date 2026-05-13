4061883327c030d75858e59ce4820450c9498c02
fikrimilano <fikrimilano1@gmail.com>
2026-05-13 18:43:29 +0700

    Migrate indexes

---
+++ engine/src/commonMain/kotlin/dev/ohs/fhir/db/impl/entities/ResourceEntity.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025-2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.ohs.fhir.db.impl.entities
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import com.google.fhir.model.r4.terminologies.ResourceType
+import kotlin.time.Instant
+import kotlin.uuid.Uuid
+
+@Entity(
+  indices =
+    [
+      Index(value = ["resourceUuid"], unique = true),
+      Index(value = ["resourceType", "resourceId"], unique = true),
+    ],
+)
+internal data class ResourceEntity(
+  @PrimaryKey(autoGenerate = true) val id: Long,
+  val resourceUuid: Uuid,
+  val resourceType: ResourceType,
+  val resourceId: String,
+  val serializedResource: String,
+  val versionId: String?,
+  val lastUpdatedRemote: Instant?,
+  val lastUpdatedLocal: Instant?,
+)
